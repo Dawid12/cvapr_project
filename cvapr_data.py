d7be3848009c39d9055047e2ab2a56b833006a5f
ScrabbleDestroyer <kristo96@op.pl>
2019-06-17 23:19:18 +0200

    Class for picutre block, data processing with mne
    load_data_from_files now returns list of custom objects, added various mne operations in various places, expanded docs, cleaned up rest of old code.
    
    The interface should not change much, although especially power spectrum related probably will at least a little. The psd_multipaper is very slow and seems not to have parameters to speed it up, will have to experiment with psd_welch.

--- cvapr_data.py
+++ cvapr_data.py
@@ -1,31 +1,45 @@
-"version 0.2.1"  # by Krzysztof Lelonek 12.05.2019
+"version 0.3.1"  # by Krzysztof Lelonek
 
-# to import this module you should add "import sys" and "sys.path.append(PATH_TO_FOLDER_WITH_THIS_MODULE)"
-#   statements at the beginning of your calling module
-# it requires Python3 and MNE https://martinos.org/mne/stable/index.html , https://pypi.org/project/mne/
-# everything that starts with _ is private, imports are imports, else is the module interface
-#   you can check what those function do in their docs
-# with default stimuli=254, each block seems to have at least 31606 samples (and it seems quite consistent),
-#   didn't check 255, you can just take first 30k samples to always pass the same count
+
+'''
+This module allows loading the data from the enterface06_EMOBRAIN database.
+It requires Python3 and MNE https://martinos.org/mne/stable/index.html , https://pypi.org/project/mne/.
+Everything that starts with _ is private, imports are imports, else is the module interface,
+you can check what those function do in their docs.
+
+
+FILTERING AND POWER SPECTRA
+
+Basic usage is to simply use load_data_from_files(numbers...) to get list of EEG samples for all blocks.
+
+Then, each of those picture block data objects can be filtered,
+or the data can also be filtered while loading (if you add keyword parameters to load_data_from_files).
+In the first case, the data is first split into blocks and then each block is filtered,
+in the second case, the data is first filtered, then split into blocks,
+and the resulting samples will be different in both cases.
+
+You can also get the power spectra from the eeg, then you always have to define range of frequencies.
+Summing up, you can:
+    - load blocks and do power spectra on each (althouh found an example where this errors)
+    - load blocks, filter each block and do power spectra on each
+    - load blocks, filtering whole file while loading, and do power spectra on each
+    - get power spectra for blocks directly from each EEG
+    - filter whole EEG and then get power spectra for blocks directly from it
+and in each case the resulting power spectra may be, sometimes even very, different.
+'''
+
 
 import os
 import mne
-import csv
-import re
 import numpy as np
+from mne.time_frequency import psd
+
 
 _env = {
     "repo_path": None,
     "bss": None,
 }
 
-def _emotion_class_to_int(emotion_class):
-    if emotion_class == "Pos":
-        return -1
-    elif emotion_class == "Neg":
-        return 0
-    else:
-        return 1
 
 def _eval_path_from_eeg_path(eeg_path):
     eeg_filename = os.path.basename(eeg_path)
@@ -35,6 +49,7 @@ def _eval_path_from_eeg_path(eeg_path):
                              *('enterface06_EMOBRAIN', 'Data', 'Common'),
                              f"Part{patient_num}SES{session_num}.log")
     return eval_path
+
 
 def _load_block_ranges_from_marker(marker_path):
     current_line = ""
@@ -54,6 +69,7 @@ def _load_block_ranges_from_marker(marker_path):
             block_ranges.append((range_start, range_end))
     return block_ranges
 
+
 def _load_evals(eval_path):
     current_line = ""
     eval_list = []
@@ -67,20 +83,89 @@ def _load_evals(eval_path):
             eval_list.append( ( int(line_parts[-2][0]), int(line_parts[-1][0]) ) )
     return eval_list
 
+
+class PictureBlockData:
+
+    '''Represents data gathered during one picture block'''
+
+    def __init__(self, single_eval, block_eeg, low_freq = None, high_freq = None):
+        '''Constructs PictureBlockData object based on data gathered from a picture block
+
+        Parameters:
+            - single_eval - tuple of patient's arousal and valence evaluation
+                (or may be other way around, it's just the way they are ordered in file)
+            - block_eeg - EEG gathered during the picture block, provided by mne library
+            - low_freq - low frequency at which the signal was filtered, in Hz
+                or None in case of no filtering
+            - high_freq - low frequency at which the signal was filtered, in Hz
+                or None in case of no filtering
+        '''
+        self._low_freq = low_freq
+        self._high_freq = high_freq
+        self._mne_eeg = block_eeg
+        self._evaluation = single_eval
+
+    @property
+    def mne_eeg(self):
+        '''Wrapped mne EEG object'''
+        return self._mne_eeg
+
+    @property
+    def evaluation(self):
+        '''Tuple of patient's arousal and valence evaluation
+        (or may be other way around, it's just the way they are ordered in file)'''
+        return self._evaluation
+
+    @property
+    def raw_eeg(self):
+        '''Samples of patient's EEG,
+        It is a np.array, with first dimension being channel number, and second - sample number'''
+        return self.mne_eeg[:,:][0]
+
+    def power_spectrum(self, low_freq, high_freq, normalize = True):
+        '''Returns a tuple,
+        where first element is a np.array
+        where i-th element is the power spectrum data for i-th frequency
+        and second element is a np.array
+        where i-th element is the i-th frequency, in Hz.'''
+        if low_freq is None:
+            low_freq = self._low_freq
+        if high_freq is None:
+            high_freq = self._high_freq
+        if normalize:
+            return psd.psd_multitaper(self.mne_eeg, self._low_freq, self._high_freq, normalization = "full")
+        else:
+            return psd.psd_multitaper(self.mne_eeg, self._low_freq, self._high_freq)
+
+    def copy(self):
+        '''Creates and returns a copy of the object'''
+        return PictureBlockData(self.single_eval, self.mne_eeg.copy(), self._low_freq, self._high_freq)
+    
+    def filter(self, low_freq = None, high_freq = None):
+        '''Filters the EEG between low_freq and high_freq (in Hz, can be None).
+        Modifies the object in place, use copy first to preserve initial object.
+        Return the object after modification.'''
+        self.mne_eeg.filter(low_freq, high_freq)
+        self._low_freq = low_freq if low_freq is not None else self._low_freq
+        self._high_freq = high_freq if high_freq is not None else self._high_freq
+        return self
+
+
 def configure(repo_path, block_start_stimuli=254):
     """Sets up parameters needed for the module to work
     
-    repo_path is the directory that containts the enterface06_EMOBRAIN directory (and everything else inside
+    repo_path is the directory that containts the enterface06_EMOBRAIN directory (and everything else inside)
     block_start_stimuli is the stimuli that indicates the beginning of the picture block
         - the other one is the end, it has to be either 254 or 255"""
 
     _env['repo_path'] = repo_path
     _env['bss'] = block_start_stimuli
 
+
 def available_eeg_paths():
-    """Returns list of paths for found eeg files
+    """Returns list of paths for found EEG files
     
-    The first patient data and second patient's first eeg are discarded"""
+    The first patient data and second patient's first EEG are discarded"""
 
     eeg_dir_path = os.path.join(_env['repo_path'], *('enterface06_EMOBRAIN', 'Data', 'EEG'))
     eeg_dir_content_list = os.listdir(eeg_dir_path)
@@ -90,24 +175,70 @@ def available_eeg_paths():
     eeg_file_path_list = [os.path.join(eeg_dir_path, eeg_file) for eeg_file in eeg_file_list]
     return eeg_file_path_list
 
-def load_data_from_files(*file_numbers):
-    '''Returns a list with each element corresponding to a picture block
-    where each element is a tuple of two elements
-    where first element is the EEG of 71 channels
-    that is a 2-dimensional array
-    where first index determines EEG channel
-    and second index determines the sample's number
-    and the second element is a tuple of two elements
-    where first element is the patient arousal evaluation
-    and second element is the patient valence evaluation
-    (or may be other way around, it's just the way they are ordered in file)
-    
-    file_numbers are indexed from 0 (to 10)'''
 
-    emotion_classes = []
-    with open(os.path.join(_env['repo_path'], 'enterface06_EMOBRAIN', 'Data', 'Common', 'IAPS_Classes_EEG_fNIRS.txt')) as emotion_classes_file:
-        for emotion_class in csv.reader(emotion_classes_file, delimiter='\t'):
-            emotion_classes.append(emotion_class)
+def load_data_from_files(*file_numbers, low_freq = None, high_freq = None):
+    '''Returns a list with PictureBlockData objects
+    where each element corresponds to one picture block.
+    
+    Parameters:
+        - file_numbers - indices of available EEG files (from 0 to 10).
+            There are loaded all blocks for each number in the order their indices are passed,
+            i.e. load_data_from_files(3,1,2,3) will first return all blocks
+            from file of index 3, then index 1, 2 and 3 again (duplicated).
+        - low_freq - low frequency used for filtering, in Hz, or None
+        - high_freq - high frequency used for filtering, in Hz, or None
+    
+    Notice that low_freq and high_freq are keyword-only parameters.'''
+
+    available_eeg_paths_snapshot = available_eeg_paths()
+    eegs_to_load = [available_eeg_paths_snapshot[i] for i in file_numbers]
+    markers_to_load = [single_eeg_to_load + '.mrk' for single_eeg_to_load in eegs_to_load]
+    evals_to_load = [_eval_path_from_eeg_path(single_eeg_to_load)
+                     for single_eeg_to_load in eegs_to_load]
+    no_filtering = low_freq is None and high_freq is None
+    loaded_data = []
+    for eeg_path, marker_path, eval_path in zip(eegs_to_load, markers_to_load, evals_to_load):
+        block_ranges = _load_block_ranges_from_marker(marker_path)  # list of tuples
+        eval_list = _load_evals(eval_path)
+        raw_eeg = mne.io.read_raw_edf(eeg_path, preload = True)
+        raw_eeg.pick_channels(raw_eeg.info["ch_names"][:-8]).filter(low_freq, high_freq)
+        for (eeg_start, eeg_end), single_eval in zip(block_ranges, eval_list):
+            block_eeg = raw_eeg.copy().crop(raw_eeg.times[eeg_start], raw_eeg.times[eeg_end])
+            picture_block_data = PictureBlockData(single_eval, block_eeg, low_freq, high_freq)
+            loaded_data.append(picture_block_data)
+    return loaded_data
+
+
+def load_power_spectra_from_files(*file_numbers, low_freq, high_freq, normalize = True, filter = False):
+    '''Returns a list,
+    where each element is a tuple corresponding to a picture block,
+    where first element is a tuple,
+    where first element is a np.array
+    where i-th element is the power spectrum data for i-th frequency
+    and second element is a np.array
+    where i-th element is the i-th frequency, in Hz
+    and second element is a tuple
+    of patient's arousal and valence evaluation
+    (or may be other way around, it's just the way they are ordered in file).
+
+    Visually:
+    [ (   ([power1, power2, ...], [freq1, freq2, ...]), (arousal, valence)   ),
+      (   ([power1, power2, ...], [freq1, freq2, ...]), (arousal, valence)   ),
+      ...
+                                                                                ]
+    Parameters:
+        - file_numbers - indices of available EEG files (from 0 to 10).
+            There are loaded all blocks for each number in the order their indices are passed,
+            i.e. load_data_from_files(3,1,2,3) will first return data for all blocks
+            from file of index 3, then index 1, 2 and 3 again (duplicated).
+        - low_freq - low frequency used for power spectra calcuation, in Hz
+        - high_freq - high frequency used for power spectra calcuation, in Hz
+        - filter - if True, the data will be explicitly filtered before power spectra calculation
+            between the given frequencies
+        - normalize - if True, the data is also normalized
+    
+    This function uses mne function to get power spectra directly from whole EEG while defining frequencies.
+    Notice that low_freq and high_freq are keyword-only and required parameters.'''
 
     available_eeg_paths_snapshot = available_eeg_paths()
     eegs_to_load = [available_eeg_paths_snapshot[i] for i in file_numbers]
@@ -118,10 +249,13 @@ def load_data_from_files(*file_numbers):
     for eeg_path, marker_path, eval_path in zip(eegs_to_load, markers_to_load, evals_to_load):
         block_ranges = _load_block_ranges_from_marker(marker_path)  # list of tuples
         eval_list = _load_evals(eval_path)
-        raw_eeg = mne.io.read_raw_edf(eeg_path, preload=True)
+        raw_eeg = mne.io.read_raw_edf(eeg_path, preload = True)
+        raw_eeg.pick_channels(raw_eeg.info["ch_names"][:-8]).filter(low_freq, high_freq)
         for (eeg_start, eeg_end), single_eval in zip(block_ranges, eval_list):
-            # raw_eeg[channel's, sample's][tuple_elem(0=samples,1=times)]
-            block_eeg = np.array(raw_eeg[0:-1, eeg_start:eeg_end + 1][0])
-            block_data = (block_eeg, single_eval)
-            loaded_data.append(block_data)
+            if normalize:
+                block_frequencies = psd.psd_multitaper(raw_eeg, low_freq, high_freq, raw_eeg.times[eeg_start], raw_eeg.times[eeg_end], normalization = "full")
+            else:
+                block_frequencies = psd.psd_multitaper(raw_eeg, low_freq, high_freq, raw_eeg.times[eeg_start], raw_eeg.times[eeg_end])
+            picture_block_data = (block_frequencies, single_eval)
+            loaded_data.append(picture_block_data)
     return loaded_data
