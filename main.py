bbdb2814968b3bb17ced0c7c45dad93b0d07df7c
ScrabbleDestroyer <kristo96@op.pl>
2019-06-25 15:31:14 +0200

    Database path from command line, no loop
    The program is now executed via command line with "python main.py <path_to_directory_containing_database_directory>".
    
    Removed the loop with repeated model creation and rest, as it was to speed up testing while minimizing keras internal random effect, the task is to actually create one neural network.

--- main.py
+++ main.py
@@ -8,6 +8,17 @@ from keras.layers import Conv1D,Conv2D, GlobalAveragePooling1D, MaxPooling1D
 import cvapr_data
 import data_processor
 import prepared_data_provider
+import sys
+import os
+
+def get_databse_path():
+    if len(sys.argv) < 2 or sys.argv[1] in ("-h", "--help"):
+        print("Usage: python main.py <path_to_dir_with_database>")
+        print("Directory <path_to_dir_with_database> has to contain enterface06_EMOBRAIN directory.")
+        return None
+    elif len(sys.argv) >= 1:
+        return sys.argv[1]
+
 #configuration
 random_seed = 42
 channel_number = 64
@@ -21,7 +32,11 @@ train_size = int((1-test_to_train)*(number_of_files*data_per_file))
 test_x = np.zeros(shape=(test_size, channel_number, freq_samples))
 test_y = []
 
-enterface06_EMOBRAIN_path = "C:\\Users\\pawel\\Documents\\Studia\\CVaPR\\Projekt"
+#enterface06_EMOBRAIN_path = "C:\\Users\\pawel\\Documents\\Studia\\CVaPR\\Projekt"
+enterface06_EMOBRAIN_path = get_databse_path()
+if enterface06_EMOBRAIN_path is None or not os.path.isdir(enterface06_EMOBRAIN_path):
+    print(f"Cannot find directory {enterface06_EMOBRAIN_path}")
+    exit(1)
 cvapr_data.configure(enterface06_EMOBRAIN_path, 254)
 prepared_data_provider.config(channel_number, freq_samples, test_size, train_size, number_of_files, test_to_train)
 random.seed(random_seed)
@@ -35,8 +50,6 @@ for i in range(number_of_files):
     (temp_train_x, temp_train_y) = prepared_data_provider.get_prepared_data(data, int(i * (test_size / number_of_files)), test_x, test_y)
     train_x.append(temp_train_x)
     train_y.append(temp_train_y)
-
-scores = []
 
 def create_model_1():  # 46.54%, 44,59% from 7 for standardize, power post split, no filtering, random 42
     model_conv = Sequential()
@@ -92,43 +105,33 @@ def create_model_5():  # 48,70%, 48,27%, 47,84%, 42,00% from 7 for standardize, 
                   metrics=['accuracy'])
     return model_conv
 
-for _ in range(7):
+model_conv = create_model_5()
 
-    model_conv = create_model_5()
+# train
+target_accuracy = 0.6
+no_improvements_iterations_limit = 10
+continue_training = True
+no_improvements_iterator = 0
+last_improved_accuracy = 0
+while continue_training:
+    train_xy = list(zip(train_x, train_y))
+    random.shuffle(train_xy)
+    train_x[:], train_y[:] = zip(*train_xy)
+    for i in range(number_of_files):
+        model_conv.fit(train_x[i], train_y[i], batch_size=len(train_x[i]), epochs=5)
 
-    # train
-    target_accuracy = 0.6
-    no_improvements_iterations_limit = 10
-    continue_training = True
-    no_improvements_iterator = 0
-    last_improved_accuracy = 0
-    while continue_training:
-        train_xy = list(zip(train_x, train_y))
-        random.shuffle(train_xy)
-        train_x[:], train_y[:] = zip(*train_xy)
-        for i in range(number_of_files):
-            model_conv.fit(train_x[i], train_y[i], batch_size=len(train_x[i]), epochs=5)
-
-        # test & check train end criteria
-        test_y = np.array(test_y)
-        score = model_conv.evaluate(test_x, test_y, batch_size=len(test_y))
-        if score[1] > target_accuracy or no_improvements_iterator > no_improvements_iterations_limit:
-            continue_training = False
-        elif score[1] > last_improved_accuracy:
-            last_improved_accuracy = score[1]
-            no_improvements_iterator = 0
-        else:
-            no_improvements_iterator += 1
-
-    #test
+    # test & check train end criteria
     test_y = np.array(test_y)
     score = model_conv.evaluate(test_x, test_y, batch_size=len(test_y))
-    print(score)
+    if score[1] > target_accuracy or no_improvements_iterator > no_improvements_iterations_limit:
+        continue_training = False
+    elif score[1] > last_improved_accuracy:
+        last_improved_accuracy = score[1]
+        no_improvements_iterator = 0
+    else:
+        no_improvements_iterator += 1
 
-    scores.append(score)
-
-scores = np.array(scores)
-model_conv.summary()
-print(scores)
-print(scores[:, 1].mean())
-print(scores[:, 1].std())
+#test
+test_y = np.array(test_y)
+score = model_conv.evaluate(test_x, test_y, batch_size=len(test_y))
+print(score)
